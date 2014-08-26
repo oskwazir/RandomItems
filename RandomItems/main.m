942e8bb874b879c8545b5eed8146625a0853d17b
Omer Wazir <omer.wazir@gmail.com>
2014-08-26 08:28:35 -0700

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Omer Wazir on 8/26/14.
+//  Copyright (c) 2014 Omer Wazir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
